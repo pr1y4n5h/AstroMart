fe0341df7356fd9f1d2b6d18320ae2508a2525ee
Priyansh Maurya <priyansh.maurya@gmail.com>
2021-06-26 16:12:45 +0530

    refactored nav bar

--- astromart/src/Components/Navbar.js
+++ astromart/src/Components/Navbar.js
@@ -4,48 +4,36 @@ import "../App.css";
 import {FaBars} from "react-icons/fa";
 import {FaRocket} from "react-icons/fa";
 
-const normalStyle = {
-    color: "#fff", 
-    textDecoration: "none", 
-    display: "flex", 
-    alignItems: "center", 
-    height: "100%", 
-    padding: "0 1rem",
-    fontSize: "1.5rem"}
-
 export function Navbar() {
+
     return ( 
     <>
     <nav className="nav-main" >
     <NavLink 
+    className="nav-btn"
     to="/"
-    style={normalStyle}
     activeStyle={{color: "#15cdfc"}}  > 
     <FaRocket />
     </NavLink>
-    <span className="nav-burger"> <FaBars /> </span>
+    <span className="nav-burger"  > <FaBars /> </span>
     <div className="nav-menu">
-        <NavLink to="/" end style={normalStyle} activeStyle={{color: "#15cdfc"}}>
+        <NavLink className="nav-btn" to="/" end activeStyle={{color: "var(--primary-color)"}}>
             Home
         </NavLink>
-        <NavLink to="/products" style={normalStyle} style={normalStyle} activeStyle={{color: "#15cdfc"}}>
+        <NavLink className="nav-btn" to="/products" activeStyle={{color: "var(--primary-color)"}}>
             Products
         </NavLink>
-        <NavLink to="/contact-us" style={normalStyle} activeStyle={{color: "#15cdfc"}}>
+        <NavLink className="nav-btn" to="/contact-us" activeStyle={{color: "var(--primary-color)"}}>
             Contact Us
         </NavLink> 
-        <NavLink to="/sign-up" style={normalStyle} activeStyle={{color: "#15cdfc"}}>
+        <NavLink className="nav-btn" to="/sign-up" activeStyle={{color: "var(--primary-color)"}}>
             Sign up
         </NavLink>
     </div>
 
-    <div className="nav-btn">
-    <NavLink className="signin-btn" to="/signin"> Sign In </NavLink>
+    <div className="signin-btn">
+    <NavLink className="signin-nav-btn" to="/signin"> Sign In </NavLink>
     </div>
-
-    {/* <NavBtn>
-        <NavBtnLink to="/signin"> Sign In </NavBtnLink>
-    </NavBtn> */}
     </nav>
     </>
     )
