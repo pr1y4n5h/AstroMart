357ca8797f69c8bb8ad39d3b21184d4d61374785
Priyansh Maurya <priyansh.maurya@gmail.com>
2021-09-13 09:28:56 +0530

    moving project to root directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "./Contexts/ThemeContext";
 import { MainProvider } from "./Contexts/MainContext";
 import { ProductProvider } from "./Contexts/ProductContext";
